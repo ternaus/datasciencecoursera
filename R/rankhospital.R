0d1c6f460e3f8f0e14547bddb41ecf96bff38a9f
Vladimir Iglovikov <iglovikov@gmail.com>
2015-01-19 12:41:51 -0800

    file for question 2 for the homework 4 for the R coursera class added

---
+++ R/rankhospital.R
@@ -0,0 +1,45 @@
+rankhospital <- function(state, outcome, num="best") {
+	#read data
+	data_file <- read.csv("outcome-of-care-measures.csv", header=T)
+	#check state
+	states <- unique(data_file$State)
+	if (!(state %in% states)) {
+		stop("invalid state")
+	}
+	#check outcome
+	outcomes <- c("heart failure", "heart attack", "pneumonia")
+	if (!(outcome %in% outcomes)) {
+		stop("invalid outcome")
+	}
+	#find best 
+	#filter state
+	data_file <- data_file[(data_file$State == state), ]
+	if (outcome == "heart attack") {		
+		data_file <- data_file[data_file$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack != "Not Available", ]
+		a <- sapply(data_file$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack, as.matrix)
+		b <- sapply(a, as.numeric)
+		data_file <- data_file[order(b, data_file$Hospital.Name), ]		
+
+	}
+	else if (outcome == "heart failure") {
+		data_file <- data_file[data_file$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure != "Not Available", ]
+		a <- sapply(data_file$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure, as.matrix)
+		b <- sapply(a, as.numeric)
+		data_file <- data_file[order(b, data_file$Hospital.Name), ]				
+	}
+	else if (outcome == "pneumonia") {
+		data_file <- data_file[data_file$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia != "Not Available", ]
+		a <- sapply(data_file$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia, as.matrix)
+		b <- sapply(a, as.numeric)		
+		data_file <- data_file[order(b, data_file$Hospital.Name), ]				
+	}	
+	if (num == "best") {
+		as.vector(data_file$Hospital.Name)[1]	
+	}
+	else if (num == "worst") {
+		as.vector(data_file$Hospital.Name)[nrow(data_file)]	
+	}
+	else {
+		as.vector(data_file$Hospital.Name)[num]
+	}
+}
